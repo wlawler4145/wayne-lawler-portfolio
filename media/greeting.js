de41e131233acb4a2496c987732acd3f751c0e9d
Wayne Lawler <wlawler4145@gmail.com>
2017-02-02 18:39:59 -0600

    Added firstName Prompt

--- media/greeting.js
+++ media/greeting.js
@@ -1,4 +1,29 @@
-var name = prompt('What is your name brotein shake?');
+var firstName = prompt('What is your first name?') || "Vistor";
+
+var lastName = prompt('What is your last name?') || "McDefaultson";
+
 var output = document.querySelector('#greeting');
 
-output.innerHTML = "<p>Thanks for visiting, " + name + ".</p>";
+if(firstName && lastName) {
+  output.innerHTML = "<p>Thanks for visiting, " + firstName + " " + lastName + ".</p>";
+} else {
+  output.innerHTML ="<p>Please tell us your name!</p>"
+
+}
+
+
+
+var richardFood = "sushi";
+var simoneFood = "Pho";
+var genaFood = "pasta";
+
+var myFoodArray = ["Tacos", "Burriotos", richardFood, "curry", simoneFood, genaFood];
+
+var classCity =
+{
+  "gena": "Ashland", "rebecca": "Memphis", "wayne": "Manchester", "richard": "Galveston", "simone": "Louisville"
+}
+
+var myCity = document.querySelector("wayne");
+
+myCity.innerHTML = "<p>wayne</p>"
